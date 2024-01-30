bb7254b68d28f448a91134489c61aebde1194cfe
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2024-01-30 15:19:40 +0400

    Initial commit

--- script.js
+++ script.js
@@ -3,19 +3,27 @@
 // Keydown event
 
 const eventFunction = (key) => {
-    // alert("event isledi !!!" + key.key)
-}
+  // alert("event isledi !!!" + key.key)
+};
 
 //KeyDown event
-document.addEventListener("keydown" , (key) => eventFunction (key));
-
+document.addEventListener("keydown", (key) => eventFunction(key));
 
 //keyUp event
-document.addEventListener("keyup" , function (key){
-    console.log('keyup -> Function running' + key.key);
-})
+document.addEventListener("keyup", function (key) {
+  console.log("keyup -> Function running" + " " + key.key);
+});
 
 //keypress event
-document.addEventListener("keypress" , (key) => {
-    console.log('keypress -> Event Clicked' + key.key);
-})
+document.addEventListener("keypress", (key) => {
+  console.log("keypress -> Event Clicked" +  " " + key.key);
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key == "y") {
+    document.write("Sehife baglanir ....")
+    setInterval(() => {
+      window.close();
+    }, 1500);
+  }
+});
